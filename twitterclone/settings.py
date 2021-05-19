9209a5329399a14b6bac76a585c2047ab66a8cb2
Soressa <78699309+Soressa@users.noreply.github.com>
2021-05-18 19:03:04 -0700

    added static root

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR /'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
+
 ]
 
 # Default primary key field type
